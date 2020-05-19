1f5ad000122c58e0d022f42da6c1761c98334006
Scott  Keplinger <skeplin17@gmail.com>
2020-05-19 21:28:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
-  # code goes here
+
+attr_accessor :owner, :mood
+attr_reader :name
+
+@@all = []
+
+  def initialize (name, owner, mood="nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+    save
+  end
+
+  # def mood=(mood)
+  #   self.mood = mood
+  # end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,83 @@
+require 'pry'
+
 class Owner
-  # code goes here
+attr_accessor
+attr_reader :name, :species
+
+@@all = []
+
+def initialize (name)
+  @name = name
+  @species = "human"
+  save
+end
+
+def save
+  @@all << self
+end
+
+def say_species
+  "I am a #{@species}."
+end
+
+def cats
+  Cat.all.select do |cats|
+    cats.owner == self
+  end
+end
+
+ def dogs
+   Dog.all.select do |dogs|
+     dogs.owner == self
+   end
+end
+
+ def buy_cat(name)
+   Cat.new(name, self)
+end
+
+ def buy_dog(name)
+   Dog.new(name, self)
+ end
+
+ def walk_dogs
+   self.dogs.each do |dog|
+     dog.mood = "happy"
+   end
+ end
+
+ def feed_cats
+   self.cats.each do |cat|
+     cat.mood = "happy"
+   end
+ end
+
+ def sell_pets
+   self.dogs.each do |dog|
+     dog.mood = "nervous"
+     dog.owner = nil
+   end
+     self.cats.each do |cat|
+       cat.mood = "nervous"
+       cat.owner = nil
+     end
+
+   end
+
+def self.all
+  @@all
+end
+
+def self.count
+  @@all.count
+end
+
+def self.reset_all
+  @@all.clear
+end
+
+def list_pets
+  puts "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  binding.pry
+end
 end
