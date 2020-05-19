f43bb38627450919e85b1d0c9be215b252589e46
Scott  Keplinger <skeplin17@gmail.com>
2020-05-19 23:09:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -77,7 +77,7 @@ def self.reset_all
 end
 
 def list_pets
-  puts "I have #{self.dogs.size} dog(s), and #{self.cats.size} cat(s)."
+ "I have #{self.dogs.size} dog(s), and #{self.cats.size} cat(s)."
 end
 
 end
